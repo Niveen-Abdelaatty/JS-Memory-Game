1b2e6feca2246805c8511d1eb629bc7911b94b83
Niveen Abdelaatty <niveen.abdelaatty@gmail.com>
2023-02-26 19:59:33 -0500

    Implement the matching feature

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
 const cards = document.querySelectorAll('.card');
 
-function flipCard(){
-    this.classList.toggle('flip');
+let isFlippedCard = false;
+let firstCard, secondCard;
+
+function flipCard() {
+  this.classList.add('flip');
+
+  if (!isFlippedCard) {
+    isFlippedCard = true;
+    firstCard = this;
+    // firstCard.removeEventListener('click', flipCard);
+  } else {
+    isFlippedCard = false;
+    secondCard = this;
+
+    if (firstCard.dataset.card === secondCard.dataset.card) {
+      firstCard.removeEventListener('click', flipCard);
+      secondCard.removeEventListener('click', flipCard);
+      console.log('matching');
+    } else {
+      setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+      }, 1500);
+    }
+  }
 }
-cards.forEach((card)=>{card.addEventListener('click', flipCard)});
+cards.forEach((card) => {
+  card.addEventListener('click', flipCard);
+});
