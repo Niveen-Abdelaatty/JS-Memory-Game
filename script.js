548cffff492959d203dbf7c55d05a16d8bebd82e
Niveen Abdelaatty <niveen.abdelaatty@gmail.com>
2023-02-26 22:03:08 -0500

    Implement Shuffling Feature

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const cards = document.querySelectorAll('.card');
+const startBtn = document.getElementById('start');
 
 let isFlippedCard = false;
 let lockBoard = false;
@@ -7,16 +8,16 @@ let secondCard;
 
 function flipCard() {
   if (lockBoard) return;
+
   this.classList.add('flip');
 
   if (!isFlippedCard) {
     isFlippedCard = true;
     firstCard = this;
-  } else {
-    isFlippedCard = false;
-    secondCard = this;
-    cardsMatching();
+    return;
   }
+  secondCard = this;
+  cardsMatching();
 }
 
 const cardsMatching = () => {
@@ -27,6 +28,8 @@ const cardsMatching = () => {
 const disableCard = () => {
   firstCard.removeEventListener('click', flipCard);
   secondCard.removeEventListener('click', flipCard);
+
+  resetBoard();
 };
 
 const unflipCards = () => {
@@ -35,10 +38,25 @@ const unflipCards = () => {
   setTimeout(() => {
     firstCard.classList.remove('flip');
     secondCard.classList.remove('flip');
-    lockBoard = false;
+    resetBoard();
   }, 1000);
 };
+
+function resetBoard() {
+  [isFlippedCard, lockBoard] = [false, false];
+  [firstCard, secondCard] = [null, null];
+}
+
+function shuffle(){
+    resetBoard();
+    cards.forEach(card => {
+        let randomOrder = Math.floor(Math.random()* 16);
+        card.style.order = randomOrder;
+    });
+}
 
 cards.forEach((card) => {
   card.addEventListener('click', flipCard);
 });
+
+startBtn.addEventListener('click', shuffle);
