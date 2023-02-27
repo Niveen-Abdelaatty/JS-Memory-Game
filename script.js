34c8311106ae797424ecc6980ca60245c3f3723a
Niveen Abdelaatty <niveen.abdelaatty@gmail.com>
2023-02-26 21:35:17 -0500

    Code Refactoring

--- script.js
+++ script.js
@@ -1,31 +1,44 @@
 const cards = document.querySelectorAll('.card');
 
 let isFlippedCard = false;
-let firstCard, secondCard;
+let lockBoard = false;
+let firstCard;
+let secondCard;
 
 function flipCard() {
+  if (lockBoard) return;
   this.classList.add('flip');
 
   if (!isFlippedCard) {
     isFlippedCard = true;
     firstCard = this;
-    // firstCard.removeEventListener('click', flipCard);
   } else {
     isFlippedCard = false;
     secondCard = this;
-
-    if (firstCard.dataset.card === secondCard.dataset.card) {
-      firstCard.removeEventListener('click', flipCard);
-      secondCard.removeEventListener('click', flipCard);
-      console.log('matching');
-    } else {
-      setTimeout(() => {
-        firstCard.classList.remove('flip');
-        secondCard.classList.remove('flip');
-      }, 1500);
-    }
+    cardsMatching();
   }
 }
+
+const cardsMatching = () => {
+  const isMatch = firstCard.dataset.card === secondCard.dataset.card;
+  isMatch ? disableCard() : unflipCards();
+};
+
+const disableCard = () => {
+  firstCard.removeEventListener('click', flipCard);
+  secondCard.removeEventListener('click', flipCard);
+};
+
+const unflipCards = () => {
+  lockBoard = true;
+
+  setTimeout(() => {
+    firstCard.classList.remove('flip');
+    secondCard.classList.remove('flip');
+    lockBoard = false;
+  }, 1000);
+};
+
 cards.forEach((card) => {
   card.addEventListener('click', flipCard);
 });
