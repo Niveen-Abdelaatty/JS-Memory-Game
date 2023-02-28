767a4b51ada350c6c5d4d6d754912ec8623f0217
Niveen Abdelaatty <niveen.abdelaatty@gmail.com>
2023-02-27 22:46:13 -0500

    Add levels & Implement medium level

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ const playerConfigOverlayElement = document.getElementById('config-overlay');
 const backdropElement = document.getElementById('backdrop');
 
 const cards = document.querySelectorAll('.card');
+const level = document.getElementById('level');
+const mediumLvlCards = document.querySelectorAll('.medium');
 const startBtn = document.getElementById('start');
-
+const refreshBtn = document.getElementById('refresh');
 const scoreElement = document.getElementById('score');
 const resultsElement = document.querySelector('.results');
 
@@ -11,7 +13,20 @@ let isFlippedCard = false;
 let lockBoard = false;
 let firstCard, secondCard, playername;
 let matches = 0;
-let levelCards = 8;
+let levelCards;
+// let levelCardArray = [];
+
+function levelSelect() {
+  const value = level.value;
+  if (value === 'easy') {
+    levelCards = 16;
+  } else if (value === 'medium') {
+    levelCards = 32;
+  } else {
+    levelCards = 48;
+  }
+  return value;
+}
 
 function flipCard() {
   if (lockBoard) return;
@@ -61,13 +76,26 @@ function resetBoard() {
   [firstCard, secondCard] = [null, null];
 }
 
+function displayLvlCards() {
+  const lvl = levelSelect();
+  console.log(lvl);
+  if (lvl === 'medium') {
+    mediumLvlCards.forEach((card) => (card.style.display = 'block'));
+  }else{
+    mediumLvlCards.forEach((card) => (card.style.display = 'none'));
+  }
+}
 function shuffle() {
+  matches = 0;
+  scoreElement.textContent = 'Score';
+  displayLvlCards();
   openPlayerConfig();
   resetBoard();
+  // console.log(cards);
   cards.forEach((card) => {
     card.classList.remove('flip');
     card.addEventListener('click', flipCard);
-    let randomOrder = Math.floor(Math.random() * 16);
+    let randomOrder = Math.floor(Math.random() * 48);
     card.style.order = randomOrder;
   });
 }
@@ -78,11 +106,11 @@ function openPlayerConfig() {
 }
 
 function whoWins() {
-  if (matches === levelCards) {
+  if (matches === levelCards/2) {
     playerConfigOverlayElement.style.display = 'none';
 
     resultsElement.style.display = 'block';
-    backdropElement.style.display = 'block';
+    backdropElement.style.display = 'none';
 
     console.log('you win');
   }
@@ -92,4 +120,6 @@ cards.forEach((card) => {
   card.addEventListener('click', flipCard);
 });
 
+level.addEventListener('change', levelSelect);
 startBtn.addEventListener('click', shuffle);
+refreshBtn.addEventListener('click', shuffle);
